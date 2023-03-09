169564dab465b2e8caef6a83c765472bc5ac1045
Bebe-Flo-So <120537192+Bebe-Flo-So@users.noreply.github.com>
2023-03-09 15:55:21 -0500

    added newItems

--- index.js
+++ index.js
@@ -18,10 +18,26 @@ object.addEventListener('dblclick', () => {
 
 }
 
+
+let sword = document.createElement ('img')
+sword.src = 'assets/sword.png'
+sword.position = 'fixed'
+sword.left = '500px'
+sword.bottom = '405px'
+document.body.append(sword)
+
+sword.addEventListener('dblclick', function() {
+    sword.remove()
+})
+
+
 newImage('assets/green-character.gif', 100, 100)
 newImage('assets/pine-tree.png', 200, 300)
 newImage('assets/pillar.png', 350, 100)
 newImage('assets/crate.png', 150, 200)
 newImage('assets/well.png', 500, 425)
+
+newItem('assets/staff.png', 600, 100)
+newItem('assets/sheild.png', 165, 185)
 
 
